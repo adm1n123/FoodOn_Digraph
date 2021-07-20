ab1b995a6a526c812826ea565bd1bd600e64d70a
4dm1n123 <deepakbaghel31@gmail.com>
2021-07-20 21:08:09 +0530

    bug fixes (300+ seeds were in non-seeds)

--- foodon.py
+++ foodon.py
@@ -156,45 +156,6 @@ class FoodOn:
         # save_pkl(digraph, self.digraph_pkl)
         return digraph
 
-    # def generate_graph(self):
-    #     print('Generating graph')
-    #     graph_dict = {class_label: idx for idx, class_label in enumerate(self.all_classes)}     # label to index
-    #     graph_dict_flip = {idx: class_label for idx, class_label in enumerate(self.all_classes)}    # index to label
-    #
-    #     G = nx.DiGraph()
-    #     for _, row in self.pd_foodon_pairs.iterrows():
-    #         if row['Parent'] in self.all_classes and row['Child'] in self.all_classes:
-    #             node_from = graph_dict[row['Parent']]
-    #             node_to = graph_dict[row['Child']]
-    #             G.add_edge(node_from, node_to)
-    #     return G, graph_dict, graph_dict_flip
-
-    # def get_candidate_classes(self):    # all classes which have at least one entity(instance) is candidate class.
-    #     print('Generating dictionary of candidate classes.')
-    #     if os.path.isfile(self.candidate_ontology_pkl) and not self.overwrite_pkl:
-    #         print('Using pre-generated candidate classes dictionary file: %s', self.candidate_ontology_pkl)
-    #         return load_pkl(self.candidate_ontology_pkl)
-    #
-    #     candidate_classes_dict = {}
-    #     for class_label in self.all_classes:
-    #         pd_match = self.pd_foodon_pairs[self.pd_foodon_pairs['Parent'] == class_label]
-    #         children = pd_match['Child'].tolist()   # children could be classes and/or entities(instances)
-    #         children_entities = [c for c in children if c in self.all_entities] # take only child entities not child classes.
-    #
-    #         if len(children_entities) > 0:
-    #             node_from = self.graph_dict['foodon product type']  # Root class for our experiments.
-    #             node_to = self.graph_dict[class_label]
-    #             paths = []
-    #             if class_label == 'foodon product type':
-    #                 paths.append(tuple(['foodon product type']))    # path from root to root
-    #             else:
-    #                 for path in nx.all_simple_paths(self.foodon_graph, source=node_from, target=node_to):   # all acyclic path from root to current class.(path includes root & current class). there could be more than one path.
-    #                     translated_path = [self.graph_dict_flip[p] for p in path]   # convert integer value of class to label.
-    #                     paths.append(tuple(translated_path[::-1]))  # reverse the path
-    #             candidate_classes_dict[class_label] = (paths, children_entities)    # for each class save all paths to root and all entities
-    #     print('Found %d candidate classes out of %d all classes.', len(candidate_classes_dict.keys()), len(self.all_classes))
-    #     save_pkl(candidate_classes_dict, self.candidate_ontology_pkl)
-    #     return candidate_classes_dict
 
     def seed_digraph(self):
         print('Seeding digraph.')
@@ -202,7 +163,7 @@ class FoodOn:
             print('Using pickled seeded digraph file: %s', self.digraph_seeded_pkl)
             return load_pkl(self.digraph_seeded_pkl)
 
-        non_seeds = set()
+        seeds = set()
         count = 0
         for _, node in self.class_dict.items():
             if len(node.all_entities) > self.num_seeds:
@@ -211,48 +172,19 @@ class FoodOn:
                 node.seed_entities = random.sample(node.all_entities, self.num_min_seeds)
             else:
                 node.seed_entities = node.all_entities.copy()
-            rest = list(set(node.all_entities) - set(node.seed_entities))
-            non_seeds = non_seeds.union(set(rest))
+            seeds = seeds.union(set(node.seed_entities))
+
             if len(node.all_entities) == 0:
                 count += 1
 
-        print(f'Classes without entities: {count}')
+        non_seeds = set(self.entity_dict.values()) - seeds
+
+        print(f'Classes without entities: {count}, classes with entities: {len(self.all_classes)-count}')
         digraph_seeded = (self.class_dict, list(non_seeds))
         # print('Saving seeded digraph to file: %s', self.digraph_seeded_pkl)
         # save_pkl(digraph_seeded, self.digraph_seeded_pkl)
         print('Found %d non-seed entities to populate out of %d all entities.' % (len(non_seeds), len(self.all_entities)))
         return digraph_seeded
-
-    # def get_seeded_skeleton(self, candidate_classes_dict):
-    #     print('Generating dictionary of skeleton candidate classes.')
-    #     if file_exists(self.skeleton_and_entities_pkl) and not self.overwrite_pkl:
-    #         print('Using pickled skeleton file: %s', self.skeleton_and_entities_pkl)
-    #         return load_pkl(self.skeleton_and_entities_pkl)
-    #
-    #     skeleton_candidate_classes_dict = {}
-    #     candidate_entities = []
-    #     for candidate_class in candidate_classes_dict.keys():   # candidate_classes_dict is [class_lable: (path, entities)]
-    #         entities = candidate_classes_dict[candidate_class][1]   # take entities of class
-    #
-    #         if len(entities) <= self.num_seeds: # if there are few entities then take fewer seeds for such classes.
-    #             temp_num_seeds = len(entities) - self.num_min_extracted_entities
-    #             if temp_num_seeds > 0:
-    #                 seeds = random.sample(entities, temp_num_seeds) # randomly sample few entities as labeled data.
-    #                 candidate_entities.extend(list(set(entities) - set(seeds))) # take candidate entities(unlabeled) to be mapped to class
-    #             else:
-    #                 seeds = entities.copy() # take all as seed no entity to populate for this class.
-    #         else:   # if there are sufficient entities
-    #             seeds = random.sample(entities, self.num_seeds)
-    #             candidate_entities.extend(list(set(entities) - set(seeds))) # take candidate entities(unlabeled) to be mapped to class
-    #
-    #         skeleton_candidate_classes_dict[candidate_class] = (candidate_classes_dict[candidate_class][0], seeds)  # store path and seeds in skeleton dict
-    #
-    #     candidate_entities = list(set(candidate_entities))  # even if an entity belongs to more than one class assume it belongs to one class and we will map it to only one class(in paper).
-    #     candidate_entities.sort()
-    #     print('Found %d candidate entities to populate out of %d all entities.', len(candidate_entities), len(self.all_entities))
-    #     return_value = (skeleton_candidate_classes_dict, candidate_entities)
-    #     save_pkl(return_value, self.skeleton_and_entities_pkl)
-    #     return return_value
 
 
     def populate_foodon_digraph(self):
@@ -267,11 +199,7 @@ class FoodOn:
             non_seeds=non_seed_entities)    # entity assumed to belongs to one class.
 
         scoring.run_config()
-
-        # scoring.calculate_precision()
-
-
-
+        return
 
 """
 All the parents of class/entity are considered.
@@ -305,3 +233,4 @@ class Entity:
         self.predicted_class = None
         self.parents = []  # all parent classes
         self.visited_classes = 0
+
